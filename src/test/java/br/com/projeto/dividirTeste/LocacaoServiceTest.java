a3587ef0ea43fa0960ff93955e466f3e13c3646a
fexx <fexx@hotmail.co.uk>
2017-11-25 18:02:01 -0200

    ExceptionParte1 - @Rule, ExpectedException, @Test(expected = Exception.class), Assert.fail, exception.expect, exception.expectMessage

--- src/test/java/br/com/projeto/dividirTeste/LocacaoServiceTest.java
+++ src/test/java/br/com/projeto/dividirTeste/LocacaoServiceTest.java
@@ -1,55 +1,111 @@
 package br.com.projeto.dividirTeste;
 
-import static br.com.projeto.utils.DataUtils.isMesmaData;
-import static br.com.projeto.utils.DataUtils.obterDataComDiferencaDias;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-import java.util.Date;
-
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.com.projeto.entidades.Filme;
-import br.com.projeto.entidades.Locacao;
 import br.com.projeto.entidades.Usuario;
 import br.com.projeto.servicos.LocacaoService;
 
+/*
+ * Algumas formas de trabalhar com exception em teste unitarios
+ */
 public class LocacaoServiceTest {
-
+	
 	/*
-	 * Rules São formas de mudar o comportamento
-	 * de um teste
+	 * Rule para se trabalhar com exeçoes em teste
 	 */
 	@Rule
-	/*
-	 * Caso queremos que em apenas um metodo tenha
-	 * varias verificação como o metodo testeLocacao()
-	 * e caso algum teste falhe e queremos mostrar todas
-	 * as verificcoes que falhou podemos adicionar todos no
-	 * ErrorCollector, nele estamos adicionando os erros
-	 * em uma coleção para separar cada erro, assim
-	 * fica quando mais de 1 teste falha, fica mais facil 
-	 * saber qual quais testes falhou 
-	 * E m um senario,como esse, que temos varias verificaçoes em um metodo.
-	 * Normalemte separamos cada verificação em um metodo.
-	 */
-	public ErrorCollector error = new ErrorCollector();
+	public ExpectedException exception = ExpectedException.none();
 	
-	@Test
-	public void testeLocacao() {
+	/*
+	 * Quando queremos testar um codigo que lança
+	 * uma exceção e caso isso aconteça o teste
+	 * passará com sucesso, pois já esta esperando a exceção
+	 * Para deixar o Junt cuidar disso
+	 * Temos que informar para ele qual exceção estamos esperando
+	 * Para isso temos que passar a excecão como parametro
+	 * na Anotação test: @Test(expected = Exception.class)
+	 * No caso abaixo, esperamos uma exeção bem generica a Exception
+	 * Mas poderia esperar uma outra qualquer .  
+	 * 
+	 */
+	@Test(expected = Exception.class)
+	public void testLocacao_filmeSemEstoque() throws Exception{
 		//cenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 2, 5.0);
+		Filme filme = new Filme("Filme 2", 0, 4.0);
 		
 		//acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
+		service.alugarFilme(usuario, filme);
+	}
+	
+	/*
+	 * Uma outra forma de trabalhar com exception em teste
+	 * É trantando manualmente, sem a ajudar do junit
+	 * No caso abaixo englobamos o metodo que lanca uma
+	 * excecao com um try catch, detro do catch verificamos
+	 * Se a mensagem da exception e a esperada
+	 * Nesse caso, vai ser um parecido com um falso positivo
+	 * Pois lancar uma excecao o vai cair no catch e no catch
+	 * Estamos verificando a mensagem de erro, com o isso
+	 * O teste passar com sucesso.
+	 * Mas se o metodo não lancar excecao ele tbm passara com sucesso
+	 * Para isso no try logo apos a chamada do metodo, temos que 
+	 * incluir um Assert.fail, informado que era para ter lancado
+	 * uma excecao
+	 * 
+	 * Caso isso aconteca o teste passara com sucesso
+	 * 
+	 */
+	@Test
+	public void testLocacao_filmeSemEstoque_2() {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 2", 0, 4.0);
 		
-		//verificacao
-		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
-		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+		//acao
+		try {
+			service.alugarFilme(usuario, filme);
+			Assert.fail("Deveria ter lancado uma excecao");
+		} catch (Exception e) {
+			assertThat(e.getMessage(), is("Filme sem estoque"));
+		}
+	}
+	
+
+	/*
+	 * Uma outra forma de trabalhar com exception em teste
+	 * É usando um novo rule de excecao
+	 * O ExpectedException exception = ExpectedException.none();
+	 * Depois precisamos informar antes da chamada do metodo que
+	 * lanca uma exceçao, as seguinte instrucoes de codigo:
+	 * exception.expect(Exception.class);
+	 * exception.expectMessage("Filme sem estoque");
+	 * 
+	 * Ou seja: queremos que o metodo a seguinte lance
+	 * As seguinte excecao, e esperamos essa mensagem
+	 * Caso isso aconteca o teste passara com sucesso
+	 * 
+	 */
+	@Test
+	public void testLocacao_filmeSemEstoque_3() throws Exception {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 2", 0, 4.0);
+		
+		exception.expect(Exception.class);
+		exception.expectMessage("Filme sem estoque");
+		
+		//acao
+		service.alugarFilme(usuario, filme);
 	}
 }
