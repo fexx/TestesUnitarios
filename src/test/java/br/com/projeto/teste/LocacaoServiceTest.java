c05cc95963507ed34c444ea1eb993025251addad
fexx <fexx@hotmail.co.uk>
2017-12-04 20:13:26 -0200

    Matchers - mastchers personalizados, TypeSafeMatcher

--- src/test/java/br/com/projeto/teste/LocacaoServiceTest.java
+++ src/test/java/br/com/projeto/teste/LocacaoServiceTest.java
@@ -1,8 +1,8 @@
 package br.com.projeto.teste;
 
-
-import static br.com.projeto.utils.DataUtils.isMesmaData;
-import static br.com.projeto.utils.DataUtils.obterDataComDiferencaDias;
+import static br.com.projeto.matchers.MatchersProprios.caiNumaSegunda;
+import static br.com.projeto.matchers.MatchersProprios.ehHoje;
+import static br.com.projeto.matchers.MatchersProprios.ehHojeComDiferencaDias;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -30,8 +30,6 @@ import br.com.projeto.utils.DataUtils;
 
 public class LocacaoServiceTest {
 
-	private static int contador = 0;
-	
 	private LocacaoService service;
 	
 	@Rule
@@ -40,32 +38,13 @@ public class LocacaoServiceTest {
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
 	
-	/* @before chama o metodo antes a cada teste ser executado
-	 * @After chama o metodo apos cada teste ser executado
-	 * @BeforeClass chama o metodo antes da classe ser instanciada, porém o metodo tem que ser static
-	 * @AfterClass chama o metodo apos a classe ser destruida, porém o metodo tem que ser static
-	 */
 	@Before
 	public void setup(){
 		service = new LocacaoService();
-		/* Para o contador ser incrementado a cada teste,
-		 * e não deixar o junit reinicializar a variavel
-		 * é necessario criar a variavel como static 
-		 */
-		contador++;
-		System.out.println(contador);
 	}
 	
 	@Test
-	public void testeLocacao() throws Exception {
-		/*
-		 * O test é ignorado caso não seja dias da semana
-		 * Caso seja sabado o test é ignorado
-		 * O assume, ele executa o teste, quando o recurso é liberado
-		 * Nesse caso é o dia, que tem que ser dias da semana para ele entrar no teste
-		 * ou seja liberar o recurso.
-		 *  
-		 */
+	public void deveAlugarFilme() throws Exception {
 		Assume.assumeFalse(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
 		
 		//cenario
@@ -76,13 +55,18 @@ public class LocacaoServiceTest {
 		Locacao locacao = service.alugarFilme(usuario, filmes);
 			
 		//verificacao
+		/*
+		 * // o metodo ehHoje é um matches personalizado
+		 * // o metodo ehHojeComDiferencaDias é um matches personalizado
+		 * 
+		 */
 		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
-		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+		error.checkThat(locacao.getDataLocacao(), ehHoje());
+		error.checkThat(locacao.getDataRetorno(), ehHojeComDiferencaDias(1));
 	}
 	
 	@Test(expected = FilmeSemEstoqueException.class)
-	public void testLocacao_filmeSemEstoque() throws Exception{
+	public void naoDeveAlugarFilmeSemEstoque() throws Exception{
 		//cenario
 		Usuario usuario = new Usuario("Usuario 1");
 		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 0, 4.0));
@@ -92,7 +76,7 @@ public class LocacaoServiceTest {
 	}
 	
 	@Test
-	public void testLocacao_usuarioVazio() throws FilmeSemEstoqueException{
+	public void naoDeveAlugarFilmeSemUsuario() throws FilmeSemEstoqueException{
 		//cenario
 		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 1, 5.0));
 		
@@ -104,10 +88,9 @@ public class LocacaoServiceTest {
 			assertThat(e.getMessage(), is("Usuario vazio"));
 		}
 	}
-	
 
 	@Test
-	public void testLocacao_FilmeVazio() throws FilmeSemEstoqueException, LocadoraException{
+	public void naoDeveAlugarFilmeSemFilme() throws FilmeSemEstoqueException, LocadoraException{
 		//cenario
 		Usuario usuario = new Usuario("Usuario 1");
 		
@@ -119,80 +102,7 @@ public class LocacaoServiceTest {
 	}
 	
 	@Test
-	public void devePagar75PctNoFilme3() throws FilmeSemEstoqueException, LocadoraException{
-		//cenario
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0), new Filme("Filme 3", 2, 4.0));
-		
-		//acao
-		Locacao resultado = service.alugarFilme(usuario, filmes);
-		
-		//verificacao
-		assertThat(resultado.getValor(), is(11.0));
-	}
-	
-
-	@Test
-	public void devePagar50PctNoFilme4() throws FilmeSemEstoqueException, LocadoraException{
-		//cenario
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(
-				new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0), new Filme("Filme 3", 2, 4.0), new Filme("Filme 4", 2, 4.0));
-		
-		//acao
-		Locacao resultado = service.alugarFilme(usuario, filmes);
-		
-		//verificacao
-		assertThat(resultado.getValor(), is(13.0));
-	}
-	
-
-	@Test
-	public void devePagar25PctNoFilme5() throws FilmeSemEstoqueException, LocadoraException{
-		//cenario
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(
-				new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0), 
-				new Filme("Filme 3", 2, 4.0), new Filme("Filme 4", 2, 4.0),
-				new Filme("Filme 5", 2, 4.0));
-		
-		//acao
-		Locacao resultado = service.alugarFilme(usuario, filmes);
-		
-		//verificacao
-		assertThat(resultado.getValor(), is(14.0));
-	}
-	
-
-	@Test
-	public void devePagar0PctNoFilme6() throws FilmeSemEstoqueException, LocadoraException{
-		//cenario
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(
-				new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0), 
-				new Filme("Filme 3", 2, 4.0), new Filme("Filme 4", 2, 4.0),
-				new Filme("Filme 5", 2, 4.0), new Filme("Filme 6", 2, 4.0));
-		
-		//acao
-		Locacao resultado = service.alugarFilme(usuario, filmes);
-		
-		//verificacao
-		assertThat(resultado.getValor(), is(14.0));
-	}
-	
-	@Test
-	/*
-	 * @Ignore É usado para ignorar um teste, ou seja o junit não roda esse teste
-	 */
 	public void deveDevolverNaSegundaAoAlugarNoSabado() throws FilmeSemEstoqueException, LocadoraException{
-		/*
-		 * O test é ignorado caso não seja sabado
-		 * Caso seja sabado o test é executado
-		 * O assume, ele executa o teste, quando o recurso é liberado
-		 * Nesse caso é o dia, que tem que ser sabado para ele entrar no teste
-		 * ou seja liberar o recurso.
-		 *  
-		 */
 		Assume.assumeTrue(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
 		
 		//cenario
@@ -203,7 +113,8 @@ public class LocacaoServiceTest {
 		Locacao retorno = service.alugarFilme(usuario, filmes);
 		
 		//verificacao
-		boolean ehSegunda = DataUtils.verificarDiaSemana(retorno.getDataRetorno(), Calendar.MONDAY);
-		Assert.assertTrue(ehSegunda);
+		// o metodo caiNumaSegunda é um matches personalizado
+		assertThat(retorno.getDataRetorno(), caiNumaSegunda());
+		
 	}
 }
